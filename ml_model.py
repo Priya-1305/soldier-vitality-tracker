4d1e9b0bf46fc34fe3d88e82b134d510197ee7ef
Priya Sirohi <142682035+Priya-1305@users.noreply.github.com>
2024-10-20 22:32:05 +0530

    Update ml_model.py

--- ml_model.py
+++ ml_model.py
@@ -1,9 +1,9 @@
 def predict_health_efficiency(data):
-    # For simplicity, let's say efficiency is based on HeartRate and BodyTemperature
+    
     heart_rate_avg = data['HeartRate'].mean()
     body_temp_avg = data['BodyTemperature'].mean()
     
-    # Simple logic for prediction (you can replace this with a real ML model)
+   
     if heart_rate_avg > 90 or body_temp_avg > 38:
         efficiency = 'Low'
     elif 70 < heart_rate_avg <= 90 and 37 < body_temp_avg <= 38:
